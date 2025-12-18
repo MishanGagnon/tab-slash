682ba8334d41bf9cded4d79126aa179f9f16cc3c
Mishan Gagnon <mishangagnon@gmail.com>
2025-12-17 21:57:41 -0800

    providers google and password, new parmas scope and google signin is primary

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,8 +1,23 @@
 import { Password } from "@convex-dev/auth/providers/Password";
-import GitHub from "@auth/core/providers/github";
 import Google from "@auth/core/providers/google";
 import { convexAuth } from "@convex-dev/auth/server";
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
-  providers: [Password, GitHub, Google],
+  providers: [
+    Password({
+      profile(params) {
+        return {
+          email: params.email as string,
+          name: params.email as string,
+        };
+      },
+    }),
+    Google({
+      authorization: {
+        params: {
+          scope: "openid email profile",
+        },
+      }
+    }),
+  ],
 });
